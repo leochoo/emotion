718473cae883de00a67c866cb54f6064f2f7728b
Leonard Choo <leochoodev@gmail.com>
2021-10-28 14:01:43 +0900

    bluetooth checked

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
-import './style.css'
+var microBitBle;
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+async function connect() {
+  microBitBle = await microBitBleFactory.connect();
+  msg.innerHTML = "micro:bit BLE接続しました。";
+}
+
+async function disconnect() {
+  await microBitBle.disconnect();
+  msg.innerHTML = "micro:bit BLE接続を切断しました。";
+}
+
+async function readSensor() {
+  var sdat = await microBitBle.readSensor();
+  console.log("sensor:", sdat);
+  isens.innerHTML =
+    "acceleration:" +
+    sdat.acceleration.x +
+    "," +
+    sdat.acceleration.y +
+    "," +
+    sdat.acceleration.z +
+    "  magneticField:" +
+    sdat.magneticField.x +
+    "," +
+    sdat.magneticField.y +
+    "," +
+    sdat.magneticField.z +
+    "," +
+    "  temperature:" +
+    sdat.temperature +
+    "  brightness:" +
+    sdat.brightness +
+    "  button:" +
+    sdat.button;
+}
+
+async function print() {
+  await microBitBle.printLED(txt.value);
+}
+
+async function showIcon() {
+  await microBitBle.showIconLED(Number(txt2.value));
+}
