319e77ae3b5902a189a57e754eb43e2afed7d79d
Leonard Choo <leochoodev@gmail.com>
2021-11-03 14:21:28 +0900

    something wrong

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+  export let name;
+  import { connect } from "./main";
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+
+  <form name="js">
+    <input type="button" value="Connect" onclick="connect();" />
+    <input type="button" value="Disconnect" onclick="disconnect();" />
+  </form>
+  <div id="msg">---</div>
+  <hr />
+  <table>
+    <tr>
+      <td colspan="2">
+        <input type="button" value="Read Sensors" onclick="readSensor();" />
+      </td>
+    </tr>
+    <tr>
+      <td>Internal Sensors</td>
+      <td id="isens">-</td>
+    </tr>
+    <tr>
+      <td colspan="2">LED</td>
+    </tr>
+    <tr>
+      <td><input id="txt" type="text" value="Hello!" /></td>
+      <td><input type="button" value="Print" onclick="print();" /></td>
+    </tr>
+    <tr>
+      <td>
+        <input id="txt2" style="width: 50px" type="text" value="0" />(0..39)
+      </td>
+      <td><input type="button" value="showIcon" onclick="showIcon();" /></td>
+    </tr>
+  </table>
+
+  <img src="imgs/CMMB_Embed.png" width="300" />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
 var microBitBle;
 
-async function connect() {
+export async function connect() {
   microBitBle = await microBitBleFactory.connect();
   msg.innerHTML = "micro:bit BLE接続しました。";
 }
 
-async function disconnect() {
+export async function disconnect() {
   await microBitBle.disconnect();
   msg.innerHTML = "micro:bit BLE接続を切断しました。";
 }
 
-async function readSensor() {
+export async function readSensor() {
   var sdat = await microBitBle.readSensor();
   console.log("sensor:", sdat);
   isens.innerHTML =
@@ -35,10 +41,12 @@ async function readSensor() {
     sdat.button;
 }
 
-async function print() {
+export async function print() {
   await microBitBle.printLED(txt.value);
 }
 
 async function showIcon() {
   await microBitBle.showIconLED(Number(txt2.value));
 }
+
+export default app;
